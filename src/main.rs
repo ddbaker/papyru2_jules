6c7d42f320ab283b5b62fa5b95e39b1b7bb307b7
Donarno Dan Baker <donarno.dan.baker@gmail.com>
2025-06-23 23:12:52 +0900

    set "egui::Theme::Light"
    modified:   Cargo.toml
    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use bevy::prelude::*;
-use bevy_egui::{egui, EguiContexts, EguiPlugin, EguiContextPass};
+use bevy_egui::{egui, EguiContextPass, EguiContexts, EguiPlugin};
 
 // This module should exist at src/easy_mark/mod.rs
 // and contain the easy_mark_editor, etc.
@@ -22,16 +22,20 @@ impl Default for EasyMarkEditorState {
 }
 
 fn main() {
+    let default_window_width = 1156.0;
+    let default_window_height = 612.0;
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "EasyMark Editor".into(),
-                resolution: (1156., 512.).into(),
+                resolution: (default_window_width, default_window_height).into(),
                 ..default()
             }),
             ..default()
         }))
-        .add_plugins(EguiPlugin { enable_multipass_for_primary_context: true, })
+        .add_plugins(EguiPlugin {
+            enable_multipass_for_primary_context: true,
+        })
         .init_resource::<EasyMarkEditorState>() // Initialize the editor state as a resource
         .add_systems(EguiContextPass, ui_system)
         .run();
@@ -40,6 +44,7 @@ fn main() {
 // Bevy system to render the Egui UI
 fn ui_system(mut contexts: EguiContexts, mut editor_state: ResMut<EasyMarkEditorState>) {
     if let Some(mut ctx) = contexts.try_ctx_mut() {
+        ctx.set_theme(egui::Theme::Light);
         egui::CentralPanel::default().show(&mut ctx, |ui| {
             editor_state.editor.ui(ui);
         });
